40f8fb86d9e6321428eef9fdcdf0ab4a0f9badb9
fumiyamiyazaki <57476802+fumiyamiyazaki@users.noreply.github.com>
2022-03-06 19:23:08 +0900

    make-atomic-design make atom,molecules,organisms,templates,pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Router } from "./router/Router";
 import "./styles.css";
 
+const user = {
+  name: "テスト",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "example@ne.jp",
+  phone: "000-9999-8888",
+  company: {
+    name: "sample会社"
+  },
+  website: "https://google.com"
+};
+
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Router />;
 }
