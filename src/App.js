023d490b1697e2f13d3323b107ec5410bcfd66f8
fumiyamiyazaki <57476802+fumiyamiyazaki@users.noreply.github.com>
2022-03-06 21:31:48 +0900

    make-pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "テスト",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "example@ne.jp",
-  phone: "000-9999-8888",
-  company: {
-    name: "sample会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
   return <Router />;
 }
